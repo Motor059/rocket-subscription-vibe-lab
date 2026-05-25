b1fc96e59bf181bf51e95d41f566a168da1db0de
Motor059 <tnsgh01@gmail.com>
2026-05-26 00:21:14 +0900

    fix: 해지 후 내역 유지되는 버그 수정

--- backend/src/main/java/com/rocket/subscription/application/dto/SubscriptionActionRequest.java
+++ backend/src/main/java/com/rocket/subscription/application/dto/SubscriptionActionRequest.java
@@ -1,10 +1,13 @@
 package com.rocket.subscription.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class SubscriptionActionRequest {
+
+    @JsonProperty("isCancelAction")
     private boolean isCancelAction; // 좌/우 스와이프 분기 플래그
 }
